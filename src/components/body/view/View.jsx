94932900790e9bb9ef56eb72f3b11767d87f9e72
Emmanuil <emmanuil.litvinenko@inbox.ru>
2024-10-18 15:03:45 +0500

    fix positions

--- src/components/body/view/View.jsx
+++ src/components/body/view/View.jsx
@@ -1,12 +1,16 @@
-import styles from './View.module.css'
 import ViewHeader from './viewHeader/ViewHeader';
 
+import styles from './View.module.css';
+
 const View = () => {
-    return (
-        <div className={styles['view-wrapper']}>
-            <ViewHeader />
-        </div>
-    );
+	return (
+		<div className={styles['view-wrapper']}>
+			<ViewHeader />
+			<div className={styles['temp-content']}>
+				<h1>Some content</h1>
+			</div>
+		</div>
+	);
 };
 
 export default View;
