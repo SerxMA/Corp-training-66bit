f937ad71ae7be2a671b4d42b64834fdf7f97eeaf
Sergey Mikhaylov <serg.m1hailov@yandex.ru>
2024-11-01 00:54:39 +0500

    little fix

--- src/api/getUserRole.js
+++ src/api/getUserRole.js
@@ -1,6 +1,6 @@
 import { makeRequest } from './makeRequest';
 
-const URL = 'user/info';
+const URL = 'userinfo';
 
 export const getUserRole = (config) =>
 	makeRequest({ method: 'GET', url: URL, ...config });
