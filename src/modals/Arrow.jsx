801fed0374f96261c9513356790e8097c849e0f5
Emmanuil <emmanuil.litvinenko@inbox.ru>
2024-11-02 16:39:03 +0500

    refactor styles on popups

--- src/modals/Arrow.jsx
+++ src/modals/Arrow.jsx
@@ -1,12 +1,21 @@
 const Arrow = () => {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
-            <path d="M5.5 12H19.5M19.5 12L13.5 18M19.5 12L13.5 6" stroke="white" 
-            strokeWidth="2" 
-            strokeLinecap="round" 
-            strokeLinejoin="round"/>
-        </svg>
-    );
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="25"
+			height="24"
+			viewBox="0 0 25 24"
+			fill="none"
+		>
+			<path
+				d="M5.5 12H19.5M19.5 12L13.5 18M19.5 12L13.5 6"
+				stroke="var(--content-constant)"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	);
 };
 
 export default Arrow;
