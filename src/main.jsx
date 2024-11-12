14c469fc41d6006578531dfd4c0902e61cde74d5
Emmanuil <emmanuil.litvinenko@inbox.ru>
2024-11-12 16:12:47 +0500

    layout a pop-up window to select a deadline

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { ruRU } from '@mui/x-date-pickers/locales';
+import 'dayjs/locale/ru';
 
 import App from './App.jsx';
 import { persistor, store } from './store/index.js';
@@ -8,7 +12,16 @@ import { persistor, store } from './store/index.js';
 createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
 		<PersistGate loading={null} persistor={persistor}>
-			<App />
+			<LocalizationProvider
+				dateAdapter={AdapterDayjs}
+				localeText={
+					ruRU.components.MuiLocalizationProvider.defaultProps
+						.localeText
+				}
+				adapterLocale="ru"
+			>
+				<App />
+			</LocalizationProvider>
 		</PersistGate>
 	</Provider>
 );
---
+++ src/modals/deadlinesPopup/DeadlinesPopup.jsx
@@ -0,0 +1,175 @@
+import { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { DateCalendar, MultiSectionDigitalClock } from '@mui/x-date-pickers';
+import dayjs from 'dayjs';
+
+import styles from './DeadlinesPopup.module.css';
+
+const formatDateTime = ({ date, time }) => {
+	const [hours, minutes] = time.split(':');
+	date.setHours(Number(hours), Number(minutes), 0);
+	return dayjs(date).format('YYYY-MM-DD HH:mm:ss');
+};
+
+const DeadlinesPopup = ({ setOpen }) => {
+	const [date, setDate] = useState('');
+	console.log(date);
+
+	const dateChange = (newTime, field) => {
+		setDate((cv) => {
+			const currDateField = cv.split(' ');
+			const revField = field === 'time' ? 'date' : 'time';
+			const revFieldContent =
+				field === 'time'
+					? currDateField.length > 1
+						? dayjs(currDateField[0]).toDate()
+						: new Date()
+					: currDateField.length > 1
+					? currDateField[1]
+					: '08:00';
+
+			const updatedDate = formatDateTime({
+				[revField]: revFieldContent,
+				[field]: newTime,
+			});
+			return updatedDate;
+		});
+	};
+
+	return ReactDOM.createPortal(
+		<div className={styles['modal-wrapper']}>
+			<div className={styles['popup']}>
+				<div className={styles['top-block']}>
+					<h2 className={styles['title']}>Название темы</h2>
+				</div>
+				<div className={styles['describe-block']}>
+					<div className={styles['data-block']}>
+						<p>Дата</p>
+						<DateCalendar
+							value={date ? dayjs(date) : null}
+							onChange={(date) =>
+								date && dateChange(date.toDate(), 'date')
+							}
+							sx={{
+								borderRadius: '0.5rem',
+								border: '1px solid var(--content-background)',
+								backgroundColor: 'var(--background-primary)',
+								color: 'var(--content-primary)',
+								boxShadow:
+									'0px 0px 16px 0px rgba(33, 37, 41, 0.06)',
+								'.css-17f9e7e-MuiTypography-root-MuiDayCalendar-weekDayLabel':
+									{
+										color: 'var(--content-disabled)',
+									},
+								'.css-cyfsxc-MuiPickersCalendarHeader-labelContainer':
+									{
+										font: 'var(--body-medium16)',
+									},
+								'.css-xb7uwb-MuiPickersArrowSwitcher-spacer': {
+									width: '30px',
+								},
+								'.css-1ckov0h-MuiSvgIcon-root': {
+									color: 'var(--content-primary)',
+								},
+								'.css-17nrfho-MuiButtonBase-root-MuiIconButton-root-MuiPickersArrowSwitcher-button':
+									{
+										border: '1px solid var(--content-border);',
+									},
+								'.css-17nrfho-MuiButtonBase-root-MuiIconButton-root-MuiPickersArrowSwitcher-button:hover':
+									{
+										backgroundColor:
+											'var(--content-background);',
+									},
+								'.css-z4ns9w-MuiButtonBase-root-MuiIconButton-root-MuiPickersArrowSwitcher-button':
+									{
+										border: '1px solid var(--content-border);',
+									},
+								'.css-z4ns9w-MuiButtonBase-root-MuiIconButton-root-MuiPickersArrowSwitcher-button:hover':
+									{
+										backgroundColor:
+											'var(--content-background);',
+									},
+								'.css-4k4mmf-MuiButtonBase-root-MuiPickersDay-root':
+									{
+										color: 'var(--content-primary);',
+									},
+								'.css-qct7wd-MuiButtonBase-root-MuiPickersDay-root':
+									{
+										color: 'var(--content-primary);',
+									},
+								'.css-qct7wd-MuiButtonBase-root-MuiPickersDay-root:not(.Mui-selected)':
+									{
+										border: '1px solid var(--content-disabled);',
+									},
+								'.css-4k4mmf-MuiButtonBase-root-MuiPickersDay-root.Mui-selected':
+									{
+										color: 'var(--content-constant)',
+									},
+								'.css-iupya1-MuiButtonBase-root-MuiIconButton-root-MuiPickersCalendarHeader-switchViewButton':
+									{
+										color: 'var(--content-subPrimary)',
+									},
+								'.css-1rf3jwr-MuiButtonBase-root-MuiIconButton-root-MuiPickersCalendarHeader-switchViewButton':
+									{
+										color: 'var(--content-subPrimary)',
+									},
+							}}
+						/>
+					</div>
+					<div className={styles['time-block']}>
+						<p>Время</p>
+						<div className={styles['time-box']}>
+							<MultiSectionDigitalClock
+								value={date ? dayjs(date) : null}
+								onChange={(time) =>
+									time &&
+									dateChange(
+										dayjs(time).format('HH:mm'),
+										'time'
+									)
+								}
+								sx={{
+									justifyContent: 'center',
+									paddingTop: '50px',
+									height: '175px',
+									'.css-ux17pc-MuiButtonBase-root-MuiMenuItem-root-MuiMultiSectionDigitalClockSection-item':
+										{
+											font: '500 1.5rem/1.25 "IBM Plex Sans"',
+											color: 'var(--blue-main)',
+										},
+									'.MuiList-root:hover::-webkit-scrollbar': {
+										display: 'none',
+									},
+									'.MuiList-root': {
+										border: 'none !important',
+									},
+								}}
+							/>
+						</div>
+					</div>
+				</div>
+				<div className={styles['btn-wrapper']}>
+					<button
+						className={`${styles.btn} ${styles.btn_cancel}`}
+						onClick={() => setOpen(false)}
+					>
+						Отмена
+					</button>
+					<button
+						className={`${styles['btn']} ${
+							date
+								? styles['btn_success']
+								: styles['btn_disabled']
+						}`}
+						disabled={!date}
+					>
+						Сохранить
+					</button>
+				</div>
+			</div>
+		</div>,
+		document.body
+	);
+};
+
+export default DeadlinesPopup;
