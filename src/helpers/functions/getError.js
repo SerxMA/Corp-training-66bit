64e7738353bc6502d214d30a5ee8b3bbd5c68794
Emmanuil <emmanuil.litvinenko@inbox.ru>
2025-01-07 22:54:20 +0500

    fast commit

--- src/helpers/functions/getError.js
+++ src/helpers/functions/getError.js
@@ -3,7 +3,6 @@ import { toast } from 'react-toastify';
 export const getError = ({ status, response }) => {
 	toast.error(
 		`Статус - ${status}\n
-		Код - ${response?.data?.error || -1}\n
 		Сообщение - "${response?.data?.message || ''}"`
 	);
 };
