24953d33319a1050886b902c007862ea8f1e5672
Emmanuil <emmanuil.litvinenko@inbox.ru>
2025-01-07 19:30:29 +0500

    add interceptor

--- src/api/makeRequest.js
+++ src/api/makeRequest.js
@@ -3,6 +3,20 @@ import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 axios.defaults.withCredentials = true;
 
+axios.interceptors.response.use(
+	(response) => response,
+	(error) => {
+		if (
+			error.response &&
+			(error.response.status === 401 || error.response.status === 403)
+		) {
+			window.location.href = '/auth';
+		}
+
+		return Promise.reject(error);
+	}
+);
+
 export const makeRequest = (config) => {
 	config.url = `${BASE_URL}/${config.url}`;
 
